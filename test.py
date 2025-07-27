67159493f57cc572672212a8eebd78ffe43ade9e
Abdelrahman <nja75753@gmail.com>
2025-07-27 17:24:17 +0300

    expand neural network comments for clarity

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 #neural network
+# A neuron takes inputs, does some math with them, and produces one output.
+# ex: a neuron that takes two inputs
+# inputs: x1, x2
+# weights: w1, w2
+# bias: b
+# output: y = w1*x1 + w2*x2 + b
